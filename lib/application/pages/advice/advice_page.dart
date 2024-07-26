a0de406a66be234b2b068e75676facab67162c59
ericknamukolo <erickmndev@gmail.com>
2024-07-27 00:59:49 +0200

    101

---
+++ lib/application/pages/advice/advice_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AdvicePage extends StatelessWidget {
+  const AdvicePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
